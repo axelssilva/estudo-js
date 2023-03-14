d302bf3b3b51b18296937af8960dec3829b59af2
Axel Silva <contato.axelsls@gmail.com>
2023-03-14 08:54:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# estudo-js
+ Estudos de JavaScript
---
+++ aula_11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velociade do seu carro é ${vel} Km/h`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade máxima, MULTADO!')
+}
+console.log('Dirija com cuidado, e use o cinto!')
