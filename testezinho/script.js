bba94b00e97f63a50731330488597a1a7a3047e2
Axel Silva <contato.axelsls@gmail.com>
2023-03-16 12:35:00 -0300

    top

--- testezinho/script.js
+++ testezinho/script.js
@@ -1,13 +1,14 @@
 function buscar() {
    let input = document.getElementById('search').value
-   input = input.toLowerCase();
-   let elemento = document.querySelectorAll('.magia');
-   for (posição = 0; posição < elemento.length; posição++) { 
-       if (!elemento[posição].innerHTML.toLowerCase().includes(input)) {
-           elemento[posição].style.display="none";
+   input = input.toLowerCase()
+   let elemento = document.querySelectorAll('.magia')
+   let titulo = document.querySelectorAll('.title')
+   for (posição = 0; posição < elemento.length; posição ++) { 
+       if (!titulo[posição].innerHTML.toLowerCase().includes(input)) {
+           elemento[posição].style.display="none"
        }
        else {
-           elemento[posição].style.display="block";                 
+           elemento[posição].style.display="block"                
        }
    }
 }
